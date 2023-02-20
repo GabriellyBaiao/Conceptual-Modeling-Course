fd3e3926fee53976b5044b0f2c5acf688538310f
Gabrielly Baiao <gabriellybaiao95@gmail.com>
2023-02-20 10:10:47 -0300

    Projeto criado

---
+++ src/test/java/com/gabriellybaiao/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellybaiao.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
